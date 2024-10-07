a87c6ad6e840d8d9c183f52d2ad6a7158ae1b946
NIKAN9024 <9024387@student.tcrmbo.nl>
2024-10-07 16:25:27 +0200

    opdracht 3 & 4

---
+++ app.js
@@ -0,0 +1,25 @@
+let heading = document.getElementById('heading');
+
+function changeText() {
+heading.innerText = "goedenavond";
+heading.style.color = "red";
+heading.style.backgroundColor = "yellow";
+}
+
+function addNumbers(num1 , num2){
+console.log(num1 + num2 = num3)
+}
+
+addNumbers(2, 7)
+
+function multiplyByFive(num) {
+  console.log(num * 5 )
+}
+
+multiplyByFive(123)
+
+function calculateMinutes(num){
+  console.log(num / 60)
+}
+  
+calculateMinutes(7200)
